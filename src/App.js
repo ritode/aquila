e9de3cce8472f3848fd3582117085255f037c000
Ritobrita De <deritobrita@gmail.com>
2023-06-12 00:14:26 +0530

    wire init

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-import DotBoard from './components/DotBoard';
+import "./App.css";
+import DotBoard from "./components/DotBoard";
 
 function App() {
   return (
     <div className="App">
       <h1>Schematic Editor</h1>
-      <div className='component'>
-         <img className='component-item' src='./circuitIcons/capacitor.png' alt='capacitor'></img>
-         <img className='component-item' src='./circuitIcons/resistor.png' alt='resistor'></img>
-         <img className='component-item' src='./circuitIcons/battery.png' alt='battery'></img>
-         <img className='component-item' src='./circuitIcons/ground.png' alt='ground'></img>
+      <div className="component">
+        <img
+          className="component-item"
+          src="./circuitIcons/capacitor.png"
+          alt="capacitor"
+        ></img>
+        <img
+          className="component-item"
+          src="./circuitIcons/resistor.png"
+          alt="resistor"
+        ></img>
+        <img
+          className="component-item"
+          src="./circuitIcons/battery.png"
+          alt="battery"
+        ></img>
+        <img
+          className="component-item"
+          src="./circuitIcons/ground.png"
+          alt="ground"
+        ></img>
       </div>
-     
-     <DotBoard w={30} h={20}/>
+
+      <DotBoard w={30} h={20} />
     </div>
   );
 }
--- src/components/DotBoard.js
+++ src/components/DotBoard.js
@@ -1,6 +1,29 @@
 import "./DotBoard.css";
+import { useEffect, useState } from "react";
 export default function DotBoard({ w, h }) {
-  function Grid({ rows, columns }) {
+  const [startPosition, setStartPosition] = useState({ x: 0, y: 0 });
+  const [endPosition, setEndPosition] = useState({ x: 0, y: 0 });
+  const [running, setRunning] = useState(false);
+
+  const handleDivClick = (event) => {
+    const xPos = event.clientX - 380; // X position of the click event relative to the viewport
+    const yPos = event.clientY - 140; // Y position of the click event relative to the viewport
+
+    console.log("Clicked at:", xPos, yPos);
+
+    if (!running) {
+      setStartPosition({ x: xPos, y: yPos });
+      setRunning(true);
+    } else {
+      setEndPosition({ x: xPos, y: yPos });
+      setRunning(false);
+    }
+  };
+  useEffect(() => {
+    console.log(startPosition, endPosition);
+  }, [startPosition, endPosition]);
+
+  function Grid({ rows, columns, handleDivClick }) {
     const gridItems = [];
 
     for (let i = 0; i < rows; i++) {
@@ -11,11 +34,25 @@ export default function DotBoard({ w, h }) {
       }
     }
 
-    return <div className="grid-container">{gridItems}</div>;
+    return (
+      <div className="grid-container" onClick={handleDivClick}>
+        {gridItems}
+      </div>
+    );
   }
   return (
     <div className="dot-board">
-      <Grid rows={w} columns={h} />
+      <Grid rows={w} columns={h} handleDivClick={handleDivClick} />
+      <svg className="line">
+        <line
+          x1={startPosition.x}
+          y1={startPosition.y}
+          x2={endPosition.x}
+          y2={endPosition.y}
+          stroke="black"
+          strokeWidth="2"
+        />
+      </svg>
     </div>
   );
 }
